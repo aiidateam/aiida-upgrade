fbfdadcb72d71130891afe1ce1614d98f558beb8
Marnik Bercx <mbercx@gmail.com>
2022-03-10 02:26:05 +0100

    ✨ NEW: Basic addition of `core.` prefix
    Add some basic refactoring code for adding the `core.` prefix to the
    deprecated `aiida-core` entry points for:
    
    * Loading a node type using one of the factories
    * Full entry points strings, including both the group and deprecated
    entry point separated by a colon, e.g.:
    
    'aiida.calculations:arithmetic.add'
    
    Also add basic CLI functionality for using `aiida-upgrade`.

--- aiida_upgrade/__main__.py
+++ aiida_upgrade/__main__.py
@@ -1,12 +1,18 @@
 """Main entry-point for CLI."""
 import sys
+from pathlib import Path
 
 import click
 
+from .migrate import migrate_path
+
 
 @click.command(context_settings={"help_option_names": ["--help"]})
-def main():
+@click.argument("path", type=click.Path(exists=True))
+def main(path):
     """The command line interface of aiida-upgrade."""
+
+    migrate_path(Path(path))
 
 
 if __name__ == "__main__":
